7a1146d9f4a325c02364766473d6840b53f5a569
changw <2283992824@qq.com>
2022-10-07 20:26:15 +0800

    仅实现了查询的前几个，增删改未写 $('.red').each(fn) 遍历每个元素并对每个元素执行fn $('#xxx').parent() 获取爸爸 $('#xxx').children()

--- dist/index.3e2f9b55.js
+++ dist/index.3e2f9b55.js
@@ -1,57 +1,9 @@
-// window.jQuery = function(selector){
-//     const elements = document.querySelectorAll(selector);
-//     //api 可以操作 elements
-//     // const api = {
-//     //     //闭包：函数访问外部变量
-//     //     addClass(className){
-//     //         //遍历所有刚才获取的元素，添加className
-//     //         for(let i=0; i<elements.length; i++){
-//     //             elements[i].classList.add(className)
-//     //         }
-//     //         // return api   //链式操作
-//     //         return this // addClass的this就是api
-//     //     }
-//     // }
-//     // return api
-//     //上面声明了一个对象叫api下面return api，是不是可以直接return对象
-//     return {
-//         //闭包：函数访问外部变量
-//         addClass(className){
-//             //遍历所有刚才获取的元素，添加className
-//             for(let i=0; i<elements.length; i++){
-//                 elements[i].classList.add(className)
-//             }
-//             // return api   //方面进行链式操作，如果没有定义api，就不能return api喽，只能用下面的this
-//         return this // addClass的this就是api，所以return api等价于return this
-//         },
-//         find(selector){
-//             //jQuery('.test1').find('.child') 需要在test1里面找child，怎样获取test1这个范围呢
-//             let array = []
-//             for(let i=0; i<elements.length; i++){
-//                 //遍历每一个test1，看里面是否有child,有将其放入array返回
-//                 const elements2 = Array.from(elements[i].querySelectorAll(selector))
-//                 array = array.concat(elements2) 
-//             }
-//             return array
-//         }
-//     }
-// }
-//上面的代码中，find不能实现链式调用，所以我们下面重新封装
-window.jQuery = function(selectorOrArray) {
+window.$ = window.jQuery = function(selectorOrArray) {
     let elements;
     if (typeof selectorOrArray === "string") elements = document.querySelectorAll(selectorOrArray);
     else if (selectorOrArray instanceof Array) elements = selectorOrArray;
     //上面声明了一个对象叫api下面return api，是不是可以直接return对象
     return {
-        oldApi: selectorOrArray.oldApi,
-        //闭包：函数访问外部变量
-        addClass (className) {
-            //遍历所有刚才获取的元素，添加className
-            for(let i = 0; i < elements.length; i++)elements[i].classList.add(className);
-            // return api   //方面进行链式操作，如果没有定义api，就不能return api喽，只能用下面的this
-            return this // addClass的this就是api，所以return api等价于return this
-            ;
-        },
         find (selector) {
             //jQuery('.test1').find('.child') 需要在test1里面找child，怎样获取test1这个范围呢
             let array = [];
@@ -66,11 +18,43 @@ window.jQuery = function(selectorOrArray) {
             ;
             return jQuery(array);
         },
+        each (fn) {
+            for(let i = 0; i < elements.length; i++)fn.call(null, elements[i], i);
+            return this;
+        },
+        parent () {
+            const array = [];
+            this.each((node)=>{
+                if (array.indexOf(node.parentNode) === -1) array.push(node.parentNode);
+            });
+            return jQuery(array);
+        },
+        children () {
+            const array = [];
+            this.each((node)=>{
+                array.push(...node.children) //相当于node.children[0],node.children[1],node.children[3]即将数组展开
+                ;
+            });
+            return jQuery(array);
+        },
+        print () {
+            console.log(elements);
+        },
+        //闭包：函数访问外部变量
+        addClass (className) {
+            //遍历所有刚才获取的元素，添加className
+            for(let i = 0; i < elements.length; i++)elements[i].classList.add(className);
+            // return api   //方面进行链式操作，如果没有定义api，就不能return api喽，只能用下面的this
+            return this // addClass的this就是api，所以return api等价于return this
+            ;
+        },
+        oldApi: selectorOrArray.oldApi,
         end () {
             return this.oldApi //this 是 新api
             ;
         }
     };
-};
+} // window.$ = window.jQuery  //可以简写成第一行的三个连等
+;
 
 //# sourceMappingURL=index.3e2f9b55.js.map
--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -5,8 +5,17 @@
 // const api1 = jQuery('.test')
 // const api2 = api1.find('.child').addClass('red').addClass('blue').addClass('green')
 // const oldApi = api2.end().addClass('yellow')
-jQuery(".test").find(".child").addClass("red").addClass("blue").addClass("green").end() //当前结束，回到上一个，即从child回到test
-.addClass("yellow") //加到上一个里面即test里面
-;
+// jQuery('.test')
+//     .find('.child')
+//     .addClass('red')
+//     .addClass('blue')
+//     .addClass('green')
+//     .end() //当前结束，回到上一个，即从child回到test
+//     .addClass('yellow') //加到上一个里面即test里面
+// const x = jQuery('.test').find('.child')
+// x.each((div)=>{console.log(div)})
+const x = jQuery(".test");
+// x.parent().print()
+x.children().print();
 
 //# sourceMappingURL=index.de158e3a.js.map
